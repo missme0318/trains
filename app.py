e8de1e1f187ef08724fe88be32e80d1ac957b45e
missme0318 <missme0318@gmail.com>
2022-06-05 10:55:42 +0800

    ADD app.py

--- app.py
+++ app.py
@@ -38,13 +38,15 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '抱歉！您說什麼？'
+    
 
     if msg in ['hi', 'Hi!']:
         r = 'Hi!'
     elif '聊天' in msg:
         r = '我是機器人'
-        
+    else:
+        r = '抱歉！您說什麼？'
+
     line_bot_api.reply_message(
         event.reply_token,
         TextSendMessage(text=r))
